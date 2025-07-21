cc2a32e0bc94739ff9a454f67692a5beee1465f7
Death1clown <goviner9723@gmail.com>
2025-07-21 16:36:59 +0100

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,10 @@
+-- Do not remove any code below this line.
+
+print("dsc.gg/rovo")
+
+print("Launching ROVO")
+
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/RGameHUB/RobloxLUA/refs/heads/master/V1.1.2/script.lua"))()
+
+
+print("Launched ROVO Sucessfuly")
