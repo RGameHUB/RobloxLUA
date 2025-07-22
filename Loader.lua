4e3c3fb0a9afe503251cdc4e1f8449972c7a90bf
Death1clown <goviner9723@gmail.com>
2025-07-22 16:16:14 +0100

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -2,9 +2,8 @@
 
 print("dsc.gg/rovo")
 
+
 print("Launching ROVO")
 
 loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/RGameHUB/RobloxLUA/refs/heads/master/V1.1.2/script.lua"))()
-
-
 print("Launched ROVO Sucessfuly")
