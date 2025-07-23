615dedd6e961a653b7ed82430895d14efdead88b
Death1clown <goviner9723@gmail.com>
2025-07-23 15:22:27 +0100

    Update Main.lua

--- Loaders/Main.lua
+++ Loaders/Main.lua
@@ -1,9 +1,1 @@
--- Do not remove any code below this line.
-
-print("dsc.gg/rovo")
-
-
-print("Launching ROVO")
-
-loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/RGameHUB/RobloxLUA/refs/heads/master/V1.1.2/script.lua"))()
-print("Launched ROVO Sucessfuly")
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/RGameHUB/RobloxLUA/refs/heads/master/Loaders/script.lua"))()
